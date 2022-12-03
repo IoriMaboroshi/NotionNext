ca40e40bb0a2fbb772866d5c6422eb7be3df94b5
伊織幻 <thechwinlyu@outlook.com>
2022-12-03 15:06:47 +0800

    Update Footer.js

--- themes/hexo/components/Footer.js
+++ themes/hexo/components/Footer.js
@@ -20,7 +20,7 @@ const Footer = ({ title }) => {
 
       <i className='fas fa-copyright' /> {`${copyrightDate}`} <span><i className='mx-1 animate-pulse fas fa-heart'/> <a href={BLOG.LINK} className='underline font-bold  dark:text-gray-300 '>{BLOG.AUTHOR}</a>.<br/>
 
-      {BLOG.BEI_AN && <><i className='fas fa-shield-alt' /> <a href='https://beian.miit.gov.cn/' className='mr-2'>{BLOG.BEI_AN}</a><br/></>}
+      {BLOG.BEI_AN && <><i className='fas fa-shield-alt' /> <a href="https://icp.gov.moe/?keyword=20224002" target="_blank"><img src="https://www.chwin.asia/icp120.png" alt="icp" width="19" height="19">萌ICP备20224002号</a><br/></>}
 
       <span className='hidden busuanzi_container_site_pv'>
             <i className='fas fa-eye'/><span className='px-1 busuanzi_value_site_pv'> </span>  </span>
