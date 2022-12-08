947a42d99a5051ac438017744f11581943d16f17
伊織幻 <thechwinlyu@outlook.com>
2022-12-08 13:01:29 +0800

    Update index.js

--- pages/category/index.js
+++ pages/category/index.js
@@ -2,35 +2,67 @@ import { getGlobalNotionData } from '@/lib/notion/getNotionData'
 import React from 'react'
 import { useGlobal } from '@/lib/global'
 import * as ThemeMap from '@/themes'
-import { getAllCategories } from '@/lib/notion/getAllCategories'
 import BLOG from '@/blog.config'
 
 /**
- * 分类首页
+ * 分类页
  * @param {*} props
  * @returns
  */
 export default function Category(props) {
   const { theme } = useGlobal()
   const ThemeComponents = ThemeMap[theme]
+  const { siteInfo, posts } = props
   const { locale } = useGlobal()
-  const { siteInfo } = props
+  if (!posts) {
+    return <ThemeComponents.Layout404 {...props} />
+  }
   const meta = {
-    title: `${locale.COMMON.CATEGORY} | ${siteInfo?.title}`,
+    title: `${props.category} | ${locale.COMMON.CATEGORY} | ${
+      siteInfo?.title || ''
+    }`,
     description: siteInfo?.description,
+    slug: 'category/' + props.category,
     image: siteInfo?.pageCover,
-    slug: 'category',
     type: 'website'
   }
-  return <ThemeComponents.LayoutCategoryIndex {...props} meta={meta} />
+  return <ThemeComponents.LayoutCategory {...props} meta={meta} />
 }
 
-export async function getStaticProps() {
-  const props = await getGlobalNotionData({ from: 'category-index-props' })
-  props.categories = getAllCategories({ allPages: props.allPages, categoryOptions: props.categoryOptions, sliceCount: 0 })
-  delete props.categoryOptions
+export async function getStaticProps({ params: { category } }) {
+  const from = 'category-props'
+  let props = await getGlobalNotionData({ from })
+
+  // 过滤状态
+  props.posts = props.allPages.filter(page => page.type === 'Post' && page.status === 'Published')
+  // 处理过滤
+  props.posts = props.posts.filter(post => post && post.category && post.category.includes(category))
+  // 处理文章页数
+  props.postCount = props.posts.length
+  // 处理分页
+  if (BLOG.POST_LIST_STYLE === 'scroll') {
+    // 滚动列表 给前端返回所有数据
+  } else if (BLOG.POST_LIST_STYLE === 'page') {
+    props.posts = props.posts?.slice(0, BLOG.POSTS_PER_PAGE - 1)
+  }
+
+  delete props.allPages
+
+  props = { ...props, category }
+
   return {
     props,
     revalidate: BLOG.NEXT_REVALIDATE_SECOND
+  }
+}
+
+export async function getStaticPaths() {
+  const from = 'category-paths'
+  const { categories } = await getGlobalNotionData({ from })
+  return {
+    paths: Object.keys(categories).map(category => ({
+      params: { category: categories[category]?.name }
+    })),
+    fallback: true
   }
 }
